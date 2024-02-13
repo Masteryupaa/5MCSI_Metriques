2d557657da120c7168b5e8cb2d351c6aa0e5b157
Masteryupaa <135553201+Masteryupaa@users.noreply.github.com>
2024-02-13 15:53:27 +0100

    Fix init.py

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
-@app.gestionerreur404(404)
+@app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
